0a132043382dc96ce7b1dcd999f2ba44760988fe
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-08 20:37:12 +0200

    Add appimage-lister-uniq.sh and lists of AppImages

---
+++ programs/appimage-lister-uniq.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIRS=$(find . -type d | grep "/" | sed 's:.*/::')
+for arch in $DIRS; do
+	ARGS=$(awk -v FS="(◆ | : )" '{print $2}' <"$arch-apps")
+	for arg in $ARGS; do
+		if grep -qe "appimage\|mage\$" "./$arch/$arg" 1> /dev/null; then
+			if ! grep "◆ $arg :" "$arch-apps" | grep -ie "\"kdegames\"\|\"kdeutils\"" 1> /dev/null; then
+				grep "◆ $arg :" "$arch-apps" >> "$arch-appimages"
+			fi
+		fi
+	done
+done
