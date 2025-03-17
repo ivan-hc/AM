18508eebab59e915ff1b4346b460563292bb7fdf
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-17 16:27:13 +0100

    Update appimage-lister-uniq.sh - create also lists of portable apps

--- programs/appimage-lister-uniq.sh
+++ programs/appimage-lister-uniq.sh
@@ -2,12 +2,16 @@
 
 DIRS=$(find . -type d | grep "/" | sed 's:.*/::')
 for arch in $DIRS; do
-	rm -f "$arch-appimages"
+	rm -f "$arch-appimages" "$arch-portable"
 	ARGS=$(awk -v FS="(◆ | : )" '{print $2}' <"$arch-apps")
 	for arg in $ARGS; do
 		if grep -qe "appimageupdatetool" "./$arch/$arg" 1>/dev/null; then
 			if ! grep "◆ $arg :" "$arch-apps" | grep -ie "\"kdegames\"\|\"kdeutils\"" 1>/dev/null; then
 				grep "◆ $arg :" "$arch-apps" >> "$arch-appimages"
+			fi
+		else
+			if ! grep "◆ $arg :" "$arch-apps" | grep -ie "\"node\"\|\"platform-tools\"\|^◆ ffwa-" 1>/dev/null; then
+				grep "◆ $arg :" "$arch-apps" >> "$arch-portable"
 			fi
 		fi
 	done
