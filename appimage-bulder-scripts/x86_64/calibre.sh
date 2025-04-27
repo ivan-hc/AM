408e8b0cdf8d6e5381760d082bc36ba340781167
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-27 23:52:11 +0200

    Update calibre.sh

--- appimage-bulder-scripts/x86_64/calibre.sh
+++ appimage-bulder-scripts/x86_64/calibre.sh
@@ -2,6 +2,35 @@
 
 APP=calibre
 
+ARCH="x86_64"
+
+# DEPENDENCIES
+
+dependencies="tar"
+for d in $dependencies; do
+	if ! command -v "$d" 1>/dev/null; then
+		echo "ERROR: missing command \"d\", install the above and retry" && exit 1
+	fi
+done
+
+_appimagetool() {
+	if ! command -v appimagetool 1>/dev/null; then
+		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
+		./appimagetool "$@"
+	else
+		appimagetool "$@"
+	fi
+}
+
+DOWNLOAD_PAGE=$(curl -Ls https://api.github.com/repos/kovidgoyal/calibre/releases)
+DOWNLOAD_URL=$(echo "$DOWNLOAD_PAGE" | sed 's/[()",{} ]/\n/g' | grep -i "http.*$ARCH.txz$" | head -1)
+VERSION=$(echo "$DOWNLOAD_URL" | tr '/-' '\n' | grep "^[0-9]")
+[ ! -f "$APP.txz" ] && curl -#Lo "$APP.txz" "$DOWNLOAD_URL"
+mkdir -p "$APP".AppDir || exit 1
+
+# Extract the package
+tar fx ./*txz* -C ./"$APP".AppDir/ || exit 1
+
 _appimage_basics() {
 	# Launcher
 	cat <<-HEREDOC >> ./"$APP".AppDir/"$APP".desktop
@@ -29,36 +58,10 @@ _appimage_basics() {
 	printf '#!/bin/sh\nHERE="$(dirname "$(readlink -f "${0}")")"\nexec "${HERE}"/%b "$@"' "$APP" > ./"$APP".AppDir/AppRun && chmod a+x ./"$APP".AppDir/AppRun
 }
 
-arch="x86_64" # Set "arm64" to build for the related architecture
-
-[ "$arch" = arm64 ] && ARCH="aarch64" || ARCH="x86_64"
-
-# DEPENDENCIES
-
-_appimagetool() {
-	if ! command -v appimagetool 1>/dev/null; then
-		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
-		./appimagetool "$@"
-	else
-		appimagetool "$@"
-	fi
-}
-
-DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/kovidgoyal/calibre/releases | sed 's/[()",{} ]/\n/g' | grep -i "http.*$arch.txz$" | head -1)
-VERSION=$(echo "$DOWNLOAD_URL" | tr '/-' '\n' | grep "^[0-9]")
-[ ! -f "$APP.txz" ] && curl -#Lo "$APP.txz" "$DOWNLOAD_URL"
-mkdir -p "$APP".AppDir || exit 1
-
-# Extract the package
-tar fx ./*txz* -C ./"$APP".AppDir/ || exit 1
 _appimage_basics
 
 # CONVERT THE APPDIR TO AN APPIMAGE
-if [ "$arch" = arm64 ]; then
-	ARCH=aarch64 VERSION="$VERSION" _appimagetool -s ./"$APP".AppDir 2>&1
-else
-	ARCH=x86_64 VERSION="$VERSION" _appimagetool -s ./"$APP".AppDir 2>&1
-fi
+ARCH=x86_64 VERSION="$VERSION" _appimagetool -s ./"$APP".AppDir 2>&1
 if ! test -f ./*.AppImage; then
 	echo "No AppImage available."; exit 1
 fi
