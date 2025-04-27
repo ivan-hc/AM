31178f59a5cc9419a51655436cf2ce5b391c5f60
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-27 23:52:38 +0200

    Update fdm.sh

--- appimage-bulder-scripts/x86_64/fdm.sh
+++ appimage-bulder-scripts/x86_64/fdm.sh
@@ -2,6 +2,35 @@
 
 APP=fdm
 
+ARCH="x86_64"
+
+# DEPENDENCIES
+
+dependencies="ar tar"
+for d in $dependencies; do
+	if ! command -v "$d" 1>/dev/null; then
+		echo "ERROR: missing command \"d\", install the above and retry" && exit 1
+	fi
+done
+
+_appimagetool() {
+	if ! command -v appimagetool 1>/dev/null; then
+		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
+		./appimagetool "$@"
+	else
+		appimagetool "$@"
+	fi
+}
+
+DOWNLOAD_PAGE=$(curl -Ls https://www.freedownloadmanager.org/download-fdm-for-linux.htm)
+DOWNLOAD_URL=$(echo "$DOWNLOAD_PAGE" | tr '">< ' '\n' | grep "http.*deb$" | grep -vi "qt" | head -1)
+VERSION=$(echo "$DOWNLOAD_PAGE" | grep "FDM [0-9]" | tr '><" ' '\n' | grep "^[0-9]" | head -1)
+[ ! -f "$APP".deb ] && curl -#Lo "$APP".deb "$DOWNLOAD_URL"
+mkdir -p "$APP".AppDir || exit 1
+
+# Extract the package
+ar x ./*.deb && tar fx ./data.tar* && mv ./opt/*/* "$APP".AppDir/ || exit 1
+
 _appimage_basics() {
 	# Launcher
 	cat <<-HEREDOC >> ./"$APP".AppDir/"$APP".desktop
@@ -22,27 +51,6 @@ _appimage_basics() {
 	printf '#!/bin/sh\nHERE="$(dirname "$(readlink -f "${0}")")"\nexec "${HERE}"/%b "$@"' "$APP" > ./"$APP".AppDir/AppRun && chmod a+x ./"$APP".AppDir/AppRun
 }
 
-ARCH="x86_64"
-
-# DEPENDENCIES
-
-_appimagetool() {
-	if ! command -v appimagetool 1>/dev/null; then
-		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
-		./appimagetool "$@"
-	else
-		appimagetool "$@"
-	fi
-}
-
-DOWNLOAD_PAGE=$(curl -Ls https://www.freedownloadmanager.org/download-fdm-for-linux.htm)
-DOWNLOAD_URL=$(echo "$DOWNLOAD_PAGE" | tr '">< ' '\n' | grep "http.*deb$" | grep -vi "qt" | head -1)
-VERSION=$(echo "$DOWNLOAD_PAGE" | grep "FDM [0-9]" | tr '><" ' '\n' | grep "^[0-9]" | head -1)
-[ ! -f "$APP".deb ] && curl -#Lo "$APP".deb "$DOWNLOAD_URL"
-mkdir -p "$APP".AppDir || exit 1
-
-# Extract the package
-command -v ar 1>/dev/null && ar x ./*.deb && command -v tar 1>/dev/null && tar fx ./data.tar* && mv ./opt/*/* "$APP".AppDir/ || exit 1
 _appimage_basics
 
 # CONVERT THE APPDIR TO AN APPIMAGE
