feec49224ded461c70394d9aeaaf0f98a8f6df87
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-27 23:53:05 +0200

    Update windows95.sh

--- appimage-bulder-scripts/x86_64/windows95.sh
+++ appimage-bulder-scripts/x86_64/windows95.sh
@@ -2,6 +2,35 @@
 
 APP=windows95
 
+ARCH="x86_64"
+
+# DEPENDENCIES
+
+dependencies="ar tar"
+for d in $dependencies; do
+	if ! command -v "$d" 1>/dev/null; then
+		echo "ERROR: missing command \"d\", install the above and retry" && exit 1
+	fi
+done
+
+_appimagetool() {
+	if ! command -v appimagetool 1>/dev/null; then
+		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
+		./appimagetool "$@"
+	else
+		appimagetool "$@"
+	fi
+}
+
+DOWNLOAD_PAGE=$(curl -Ls https://raw.githubusercontent.com/felixrieseberg/windows95/refs/heads/master/README.md)
+DOWNLOAD_URL=$(echo "$DOWNLOAD_PAGE" | tr '><" ' '\n' |  grep -i "^https.*github.com.*download.*deb$" | grep -i "x64\|amd64\|x86_64" | head -1)
+VERSION=$(echo "$DOWNLOAD_URL" | tr '_' '\n' | grep "^[0-9]" | head -1)
+[ ! -f "$APP".deb ] && curl -#Lo "$APP".deb "$DOWNLOAD_URL"
+mkdir -p "$APP".AppDir/usr || exit 1
+
+# Extract the package
+ar x ./*.deb && tar fx ./data.tar* && mv ./usr/* "$APP".AppDir/usr/ || exit 1
+
 _appimage_basics() {
 	# Launcher
 	cat <<-HEREDOC >> ./"$APP".AppDir/"$APP".desktop
@@ -23,27 +52,6 @@ _appimage_basics() {
 	printf '#!/bin/sh\nHERE="$(dirname "$(readlink -f "${0}")")"\nexec "${HERE}"/usr/bin/%b "$@"' "$APP" > ./"$APP".AppDir/AppRun && chmod a+x ./"$APP".AppDir/AppRun
 }
 
-ARCH="x86_64"
-
-# DEPENDENCIES
-
-_appimagetool() {
-	if ! command -v appimagetool 1>/dev/null; then
-		[ ! -f ./appimagetool ] && curl -#Lo appimagetool https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-"$ARCH".AppImage && chmod a+x ./appimagetool
-		./appimagetool "$@"
-	else
-		appimagetool "$@"
-	fi
-}
-
-DOWNLOAD_PAGE=$(curl -Ls https://raw.githubusercontent.com/felixrieseberg/windows95/refs/heads/master/README.md)
-DOWNLOAD_URL=$(echo "$DOWNLOAD_PAGE" | tr '><" ' '\n' |  grep -i "^https.*github.com.*download.*deb$" | grep -i "x64\|amd64\|x86_64" | head -1)
-VERSION=$(echo "$DOWNLOAD_URL" | tr '_' '\n' | grep "^[0-9]" | head -1)
-[ ! -f "$APP".deb ] && curl -#Lo "$APP".deb "$DOWNLOAD_URL"
-mkdir -p "$APP".AppDir/usr || exit 1
-
-# Extract the package
-command -v ar 1>/dev/null && ar x ./*.deb && command -v tar 1>/dev/null && tar fx ./data.tar* && mv ./usr/* "$APP".AppDir/usr/ || exit 1
 _appimage_basics
 
 # CONVERT THE APPDIR TO AN APPIMAGE
