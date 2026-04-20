f4e5b1c8c11911327f9e0df02967da17d41e0674
Vishnu <5123344+vishnu350@users.noreply.github.com>
2026-04-20 18:09:20 +0800

    Test improvements: - Updated install test to check for outdated apps feature. - Made am -f table checking more robust.

--- regress/test-am-install.sh
+++ regress/test-am-install.sh
@@ -6,6 +6,7 @@
 # Test variables
 test_results=".results.tmp"
 app_name=$(_pick_random_app "$TEST_APP_LIST_ZIP")
+app_name_old=$(_pick_random_app "$TEST_APP_LIST_OLD")
 
 ## Setup
 _log "Running multi-user install test: $0"
@@ -25,7 +26,7 @@ am -i "$app_name"
 _log "Checking if $app_name is installed in both user/system modes..."
 am --system
 am -f > "$test_results"
-_check_count "$app_name" 2 "$test_results"
+_check_count "$app_name.*MiB" 2 "$test_results"
 
 # Remove in User Mode
 _log "Removing $app_name in user mode..."
@@ -37,7 +38,7 @@ am -r "$app_name"
 # Check for Errors
 _log "Checking if $app_name is removed in user mode..."
 am -f > "$test_results"
-_check_count "$app_name" 0 "$test_results"
+_check_count "$app_name.*MiB" 0 "$test_results"
 
 # Reinstall in User Mode
 _log "Installing $app_name in user mode again..."
@@ -53,7 +54,17 @@ am -R "$app_name"
 # Check for Errors
 _log "Checking if $app_name is completely removed in system mode..."
 am -f > "$test_results"
-_check_count "$app_name" 0 "$test_results"
+_check_count "$app_name.*MiB" 0 "$test_results"
+
+# Install old app in both modes
+_log "Installing $app_name_old (outdated) in both system/user modes..."
+am -i "$app_name_old"
+am -i --user "$app_name_old"
+
+# Check for "old release msg"
+_log "Checking if $app_name_old (outdated) is listed in both system/user modes with warnings..."
+am -f > "$test_results"
+_check_count "$app_name_old" 4 "$test_results"
 
 # Pass the test
 _remove_all_apps
