44eecbaa18c6a568ff41e05d1f5b05e3755f5b36
Vishnu <5123344+vishnu350@users.noreply.github.com>
2026-04-23 01:24:54 +0800

    Added nolibfuse test.

---
+++ regress/test-am-nolibfuse.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# Source common test functions
+. "$(dirname "$0")/test-common.sh"
+
+# Test variables
+test_results=".results.tmp"
+app_name="bench-cli"
+
+## Setup
+_log "Running libfuse2 removal test: $0"
+am --system
+
+# Installing a known archieved tool command line tool using libfuse2
+_log "Installing a known libfuse2 app..."
+am -R "$app_name"
+am -i "$app_name"
+
+# Test app
+_log "Testing app to see libfuse2 error..."
+"$app_name" 2> "$test_results"
+_check_count "error.*libfuse" 1 "$test_results"
+
+# Remove libfuse2 dependency
+_log "Removing libfuse2 dependency..."
+printf "y\n" |\
+am nolibfuse "$app_name"
+
+# Test app
+_log "Testing app to see if libfuse2 error is gone..."
+"$app_name" 2> "$test_results"
+_check_count "error.*libfuse" 0 "$test_results"
+
+# Pass the test
+_remove_all_apps
+_pass
+
