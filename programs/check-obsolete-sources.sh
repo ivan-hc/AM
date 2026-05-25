da4fc77869603830e1e8005dfe04dec003889952
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-05-25 16:19:40 +0200

    Update check-obsolete-sources.sh

--- programs/check-obsolete-sources.sh
+++ programs/check-obsolete-sources.sh
@@ -4,17 +4,35 @@ obsolescence_threshold=$(date -u -d "1 years ago" +"%Y")
 
 _archived_list() {
 	user_repo=$(grep "^SITE=" "$arg" | head -1 | awk -F'"' '/^SITE=/{print $2}')
-	if curl -s "https://api.gh.pkgforge.dev/repos/$user_repo" | grep -q '"archived".* true,'; then
-		echo "$arg" | sed s:.*/:: >> archived-list.tmp
-	fi
+	TAKES_COUNT=0
+	while [ "$TAKES_COUNT" -lt 50 ]; do
+		repo=$(curl -s "https://api.gh.pkgforge.dev/repos/$user_repo")
+		if [ -n "$repo" ]; then
+			if echo "$repo" | grep -q '"archived".* true,'; then
+				echo "$arg" | sed s:.*/:: >> archived-list.tmp
+				echo " - $arg is Archived"
+			fi
+		else
+			TAKES_COUNT=$((TAKES_COUNT + 1))
+		fi
+	done
+	repo=""
 }
 
 _obsolete_list() {
 	user_repo=$(grep "^SITE=" "$arg" | head -1 | awk -F'"' '/^SITE=/{print $2}')
-	date=$(curl -s "https://api.gh.pkgforge.dev/repos/$user_repo/releases" | grep -oP '"updated_at": "\K\d{4}' | sort -n | tail -1)
-	if [ -n "$date" ] && echo "$date" | grep -qE '^[0-9]{4}$' && [ "$date" -lt "$obsolescence_threshold" ]; then
-		echo "$arg $date" | sed s:.*/:: >> obsolete-list.tmp
-	fi
+	TAKES_COUNT=0
+	while [ "$TAKES_COUNT" -lt 50 ]; do
+		date=$(curl -s "https://api.gh.pkgforge.dev/repos/$user_repo/releases" | grep -oP '"updated_at": "\K\d{4}' | sort -n | tail -1)
+		if [ -n "$date" ]; then
+			if echo "$date" | grep -qE '^[0-9]{4}$' && [ "$date" -lt "$obsolescence_threshold" ]; then
+				echo "$arg $date" | sed s:.*/:: >> obsolete-list.tmp
+				echo " - $arg $date"
+			fi
+		else
+			TAKES_COUNT=$((TAKES_COUNT + 1))
+		fi
+	done
 	date=""
 }
 
