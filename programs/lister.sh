78897349b77bda681e19f0bf0ab3f14d9f0ba5c3
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-05-27 22:21:36 +0200

    Update lister.sh

--- programs/lister.sh
+++ programs/lister.sh
@@ -8,15 +8,22 @@ for arch in $DIRS; do
 		if [ -f "./$arch/$arg" ]; then
 			grep "^◆ $arg :" "$arch-apps" | head -1 >> "$arch-tmplist"
 			if grep -qe "appimageupdatetool" "./$arch/$arg" 1>/dev/null; then
-				if ! grep "◆ $arg :" "$arch-apps" | grep -ie "\"kdegames\"\|\"kdeutils\"" 1>/dev/null; then
-					grep "◆ $arg :" "$arch-apps" | head -1 >> "$arch-appimages"
-				fi
+				grep "◆ $arg :" "$arch-apps" | head -1 >> "$arch-appimages"
 			else
-				if ! grep "◆ $arg :" "$arch-apps" | grep -ie "\"node\"\|\"platform-tools\"" 1>/dev/null; then
-					grep "◆ $arg :" "$arch-apps" | head -1 >> "$arch-portable"
-				fi
+				grep "◆ $arg :" "$arch-apps" | head -1 >> "$arch-portable"
 			fi
 		fi
 	done
-	[ -f "$arch-tmplist" ] && sort "$arch-tmplist" > "$arch-apps"
+	METAPACKAGES="kdegames kdeutils node platform-tools"
+	for m in $METAPACKAGES; do
+		metapkg_page=$(curl -Ls --retry 5 --retry-max-time 120 "https://portable-linux-apps.github.io/apps/$m.md" 2>/dev/null)
+		if [ -z "$metapkg_page" ]; then
+			exit 1
+		elif ! echo "$metapkg_page" | head -1 | grep -qi "^# $m"; then
+			exit 1
+		else
+			echo "$metapkg_page" | grep -- " - .* : .*.$" | sed -- "s/^ - / ◆ /g; s/$/ This is part of \"$m\"./g" >> "$arch-tmplist"
+		fi
+	done
+	[ -f "$arch-tmplist" ] && sort -u "$arch-tmplist" > "$arch-apps"
 done
