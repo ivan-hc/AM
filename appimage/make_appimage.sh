6402a3765853fa670700c7067ce01e913c71845e
Vishnu <5123344+vishnu350@users.noreply.github.com>
2026-09-13 04:42:01 +0800

    AM AppImage commits: - Added AppImage awareness to APP-MANAGER. - Added AppImmage awareness to the install scripts. - Added appimage dir containing AppRun, Appimage build scripts and .desktop file.

---
+++ appimage/make_appimage.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -eu
+
+# Parameters
+ARCH=$1
+VERSION=$2
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.hook"
+export GITHUB_REPOSITORY="https://github.com/ivan-hc/AM"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=logo.svg
+export DESKTOP=appimage/APP-MANAGER.desktop
+export MAIN_BIN=AppRun
+export APPDIR="AppDir-$VERSION-$ARCH"
+
+# Prepare files
+rm -Rf "$APPDIR"/*
+mkdir -p "$APPDIR"/bin "$APPDIR"/share
+# Copy AM files
+cp appimage/AppRun "$APPDIR"/.
+cp APP-MANAGER "$APPDIR"/bin/.
+cp AM-INSTALLER "$APPDIR"/bin/.
+cp INSTALL "$APPDIR"/bin/INSTALL-AM-APPIMAGE.sh
+cp -rf modules "$APPDIR"/share/.
+# Copy coreutils
+#wget -q https://github.com/ivan-hc/am-utils/releases/download/continuous-"$ARCH"/chown-"$ARCH"-static -O "$APPDIR"/bin/chown
+cp /bin/curl  "$APPDIR"/bin/curl
+cp /bin/grep  "$APPDIR"/bin/grep
+cp /bin/sed   "$APPDIR"/bin/sed
+cp /bin/cat   "$APPDIR"/bin/cat
+cp /bin/chmod "$APPDIR"/bin/chmod
+cp /bin/chown "$APPDIR"/bin/chown
+chmod a+x "$APPDIR"/bin/*
+
+# Deploy dependencies
+appimage/quick-sharun.sh "$APPDIR"
+
+# Turn AppDir into AppImage
+appimage/quick-sharun.sh --make-appimage
+
+# Test the app for 12 seconds
+appimage/quick-sharun.sh --simple-test ./dist/*.AppImage
+
